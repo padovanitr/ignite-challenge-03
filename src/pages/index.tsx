6fc652347f2e113bc8c82da1c82cf42cbb596352
Thiago Padovani <padovanitr@gmail.com>
2022-01-09 20:12:54 -0300

    set up first basic structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,13 +24,15 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <h1>Home</h1>
+  )
+}
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+//export const getStaticProps = async () => {
+//  const prismic = getPrismicClient();
+  //const postsResponse = await prismic.query(TODO);
 
-//   // TODO
-// };
+  // TODO
+//};
