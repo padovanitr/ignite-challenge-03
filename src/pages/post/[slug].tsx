5cf0ad3aaab818a9c0f49571fdce2bbe8a1da437
Thiago Padovani <padovanitr@gmail.com>
2022-01-11 22:57:38 -0300

    structure and styles post page

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,14 +1,19 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 
+import { FiCalendar, FiUser, FiClock } from 'react-icons/fi';
 import { getPrismicClient } from '../../services/prismic';
 import { RichText } from 'prismic-dom';
-//import { Link, RichText } from 'prismic-reactjs'
+import * as prismicH from '@prismicio/helpers';
+import { format, parseISO } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
+import Head from 'next/head';
+import { Header } from '../../components/Header';
 
 interface Post {
-  first_publication_date: string | null;
+  last_publication_date: string | null;
   data: {
     title: string;
     banner: {
@@ -28,20 +33,52 @@ interface PostProps {
   post: Post;
 }
 
-const linkResolver = ({post}) => {
-  // Pretty URLs for known types
-  if (post.type === 'blog') return `/post/${post.uid}`
-  if (post.type === 'page') return `/${post.uid}`
-  // Fallback for other types, in case new custom types get created
-  return `/post/${post.id}`
-}
+export default function Post({ post }: PostProps) {
+    console.log('post', post)
 
-export default function Post({ post }) {
-  console.log('post', post)
-   // TODO
    return (
      <>
+      <Head>
+        <title>{post.data.title} | Space Traveling</title>
+      </Head>
 
+      {post.data.banner.url && (
+        <section className={styles.banner}>
+          <img src={post.data.banner.url} alt="Banner" />
+        </section>
+      )}
+
+      <main className={styles.content}>
+        <article className={styles.post}>
+          <h1>{post.data.title}</h1>
+
+          <div className={styles.postInfo}>
+            <span>
+              <FiCalendar size={20} color="#BBBBBB" />
+              {post.last_publication_date}
+            </span>
+
+            <span>
+              <FiUser size={20} color="#BBBBBB" />
+              {post.data.author}
+            </span>
+          </div>
+
+          <div className={styles.postContent}>
+            {post.data.content.map(({ heading, body }) => (
+              <div key={heading}>
+                {heading && <h2>{heading}</h2>}
+
+                <div
+                  className={styles.postSection}
+                  // eslint-disable-next-line react/no-danger
+                  dangerouslySetInnerHTML={{ __html: RichText.asHtml(body) }}
+                />
+              </div>
+            ))}
+          </div>
+        </article>
+      </main>
      </>
    )
 }
@@ -60,18 +97,22 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 
   const response = await prismic.getByUID('post', String(slug), {});
 
-  console.log(response.data.content);
+  console.log('content', prismicH.asText(response.data['content']))
 
   const post = {
-    slug,
-    title: response.data['title'],
-    author: response.data['author'],
-    banner: response.data['banner'],
-    updatedAt: new Date(response.last_publication_date).toLocaleString('pt-BR', {
-        day: '2-digit',
-        month: 'long',
-        year: 'numeric'
-    })
+    uid: response.uid,
+    last_publication_date: new Date(response.last_publication_date).toLocaleString('pt-BR', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric'
+    }),
+    data: {
+      title: response.data.title,
+      subtitle: response.data.subtitle,
+      banner: response.data.banner,
+      author: response.data.author,
+      content: response.data.content,
+    },
   }
 
   return {
